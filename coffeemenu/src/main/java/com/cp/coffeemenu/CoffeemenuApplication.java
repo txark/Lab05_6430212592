65ae8827079865bf26c8b15f873246253c04c2d7
Txark <artitaya.tk@gmail.com>
2026-07-28 11:49:29 +0700

    .
    Create Folder "Lab05" & Project file "coffeemenu"

---
+++ coffeemenu/src/main/java/com/cp/coffeemenu/CoffeemenuApplication.java
@@ -0,0 +1,13 @@
+package com.cp.coffeemenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeemenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeemenuApplication.class, args);
+	}
+
+}
