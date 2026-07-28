84f39aac123cf67195b1e537315a45f45b5a1ae4
Txark <artitaya.tk@gmail.com>
2026-07-28 12:12:06 +0700

    add
    Create "Model" , "Service" & "Controller" Folder

---
+++ coffeemenu/src/main/java/com/cp/coffeemenu/Controller/CoffeeController.java
@@ -0,0 +1,52 @@
+package com.cp.coffeemenu.Controller;
+
+import com.cp.coffeemenu.model.Coffee;
+import com.cp.coffeemenu.service.CoffeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/coffees")
+public class CoffeeController {
+    private final CoffeeService coffeeService;
+
+    puclic CoffeeController(CoffeeService coffeeService) {
+        this.coffeeService = coffeeService;
+    }
+
+    @GetMapping
+    public List<Coffee> getAllCoffees() {
+        return coffeeService.getAllCoffees();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Coffee> getCoffeeById(@PathVariable Long id) {
+        return coffeeService.getCoffeeById(id)
+                .map(coffee -> new ResponseEntity<>(coffee, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Coffee> addCoffee(@RequestBody Coffee coffee) {
+        Coffee created = coffeeService.addCoffee(coffee);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Coffee> updateCoffee(@PathVariable Long id, @RequestBody Coffee coffee) {
+        return coffeeService.updateCoffee(id, coffee)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCoffee(@PathVariable Long id) {
+        if (coffeeService.deleteCoffee(id)) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
